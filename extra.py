b56d82248e4b9d7ae84ec0b727ebd710e620c94f
SamuelBabs24 <108993793+SamuelBabs24@users.noreply.github.com>
2022-07-09 18:03:49 +0100

    Add files via upload

---
+++ extra.py
@@ -0,0 +1,9 @@
+number = list(range(20))
+number = 20
+for number in range(number):
+    while number >= 0:
+        print(number)
+        break
+    
+
+    
---
+++ finder.py
@@ -0,0 +1,8 @@
+def finder(mylist, target):
+    if target in mylist:
+        print("True")
+    else:
+        print("False")
+
+numbers = [1, 2, 3, 4, 5]
+finder(numbers, 8)
