515de32385b203836f52647bd98c9ed082a65c9c
SamuelBabs24 <108993793+SamuelBabs24@users.noreply.github.com>
2022-07-09 17:59:25 +0100

    Upload reverse_word script

---
+++ reverse_word.py
@@ -0,0 +1,13 @@
+#Write a program to reverse a word (The Factory)
+#letters = ['T', 'h', 'e', 'F', 'a', 'c', 't', 'o', 'r', 'y']
+#print(letters[::-1])
+
+"""
+letters = "The Factory"
+for character in letters:
+    print(character)
+"""
+
+limit = 100
+for number in range(limit):
+    print(number)
