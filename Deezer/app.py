fa637de6193a4ef3690b41480bd1d6d4d7518876
Femprogs <98106415+Femprogs@users.noreply.github.com>
2023-05-20 17:11:23 +0100

    Artist search on deezer

---
+++ Deezer/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, url_for, jsonify, redirect
+import requests
+
+app = Flask(__name__)
+
+
+DEEZER_API_URL = 'https://api.deezer.com'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form['query']
+
+    response = requests.get(f'{DEEZER_API_URL}/search/artist?q={query}')
+    artists = response.json()['data']
+
+    # Render the search_results.html template and pass the artists data
+    return render_template('search_results.html', artists=artists)
+
+
+if __name__ == '__main__':
+    app.run()
