be9d3016d6ea60a5bbbd61e0195e31088639405b
Daniel <derrysantaclos@gmail.com>
2023-05-27 17:32:57 +0100

    include comments

--- back-end/app.py
+++ back-end/app.py
@@ -14,18 +14,29 @@ api_url = "https://api.deezer.com/search/artist?q=eminem&limit=1"
 @app.route("/", methods=['POST', 'GET'])
 # @cross_origin
 def home():
-    artist_info = {}
-    artist_info['top_5_tracks'] = {}
+   
     if request.method == 'POST':
         request_data = request.form
+        #get artist name from front-end/ templates  
         artist_name = request_data['artist_name']
-        print("good")
+        #the deezer api
         api_url = f"https://api.deezer.com/search/artist?q={artist_name}&limit=1"
         artist_data = requests.get(api_url).json()
+         #create an empty dict to store our result object - artist_info
+        artist_info = {}
 
+        #include a new key for top 5 artist track in artist info
+        artist_info['top_5_tracks'] = {}
+
+        #confirms there is a result before attempting to retrieve anything
         if artist_data['data'] != []:
+          #print(artist_data) you will understand previous line better
+          #you would also get insight to next line : working with list and dictonary
             artist_details = artist_data['data'][0]
+            #print(artist_details) to get familiar with what you working with
 
+
+            #include copy some needed items from api object i.e artist_info to the artist_details we would return/render in the front-end/templates
             artist_info['artist_image'] = artist_details['picture_medium']
             artist_info['artist_link'] = artist_details['link']
             artist_info['artist_name'] = artist_name
